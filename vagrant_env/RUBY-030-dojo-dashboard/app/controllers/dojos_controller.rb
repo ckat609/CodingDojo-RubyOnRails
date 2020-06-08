b532174194e02c4631160302f3d4c7c978ea407b
Armando Tello <ckat609@yahoo.com>
2020-06-08 14:26:01 -0500

    finished dojo dashboard ii

--- vagrant_env/RUBY-030-dojo-dashboard/app/controllers/dojos_controller.rb
+++ vagrant_env/RUBY-030-dojo-dashboard/app/controllers/dojos_controller.rb
@@ -4,6 +4,7 @@ class DojosController < ApplicationController
   end
 
   def new
+
   end
 
   def show
@@ -13,11 +14,26 @@ class DojosController < ApplicationController
   end
 
   def create
+    @aNewDojo = Dojo.create(dojo_params)
+    
+    if @aNewDojo.valid?
+      flash[:success] = "#{params[:dojo][:branch]} Dojo - #{params[:dojo][:street]} #{params[:dojo][:city]},#{params[:dojo][:state]} was added successfully!"
+      redirect_to "/"
+    else
+      flash[:error] = @aNewDojo.errors.full_messages
+      redirect_to "/dojos/new"
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def dojo_params
+    params.require(:dojo).permit(:branch, :street, :city, :state)
   end
 end
--- vagrant_env/RUBY-030-dojo-dashboard/config/routes.rb
+++ vagrant_env/RUBY-030-dojo-dashboard/config/routes.rb
@@ -9,11 +9,11 @@ Rails.application.routes.draw do
 
   get 'dojos/edit'
 
-  get 'dojos/create'
+  post 'dojos/create'
 
-  get 'dojos/update'
+  post 'dojos/update'
 
-  get 'dojos/destroy'
+  post 'dojos/destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
